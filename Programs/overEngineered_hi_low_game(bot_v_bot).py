84620abb48cb54f98fd6b3adfc9988efdb2a6f89
museHD <57558058+museHD@users.noreply.github.com>
2021-02-13 02:04:34 +1100

    Added comments to HiLow

--- Programs/overEngineered_hi_low_game(bot_v_bot).py
+++ Programs/overEngineered_hi_low_game(bot_v_bot).py
@@ -13,7 +13,7 @@ class Player(object):
 	def sendMessage(self, message):
 		print("Player: {0}".format(message))
 
-	# 
+	# Binary Search (finds the average of the boundaries of the range)
 	def guessNum(self):
 		return(int((self.min+self.max)/2))
 
@@ -21,6 +21,7 @@ class Player(object):
 		self.myGuess = self.guessNum()
 		self.sendMessage("I know that the range is {0} so I will guess {1}".format(self.max,self.myGuess))
 		
+	# Checks whether result is higher or lower and guesses accordingly
 	def guess(self, result):
 
 		if result == "HIGHER":
@@ -42,13 +43,16 @@ class Host(object):
 		self.secret = 0
 		self.endgame = False
 
+	# Function to print message with a prefix
 	def sendMessage(self, message):
 		print("Host: {0}".format(message))
 
+	# Generate random number
 	def randomGen(self):
 		self.secret = random.randint(0, self.range)
 		self.sendMessage(("Shhh! The secret number is {0}".format(self.secret)))
 
+	# Checks player input and returns HIGHER or LOWER or ends the game for correct answer
 	def check(self, guess):
 		if guess == self.secret:
 			self.sendMessage("Good Job! You got it right. Only took you {0} tries".format(self.tries))
@@ -70,7 +74,7 @@ try:
 except:
 	print("Thats not a proper number!")
 
-
+# Instantiate classes and call their basic functions
 myHost = Host(theRange)
 myHost.randomGen()
 
@@ -78,6 +82,7 @@ myPlayer = Player(theRange)
 myPlayer.firstGuess()
 myHost.check(myPlayer.myGuess)
 
+# Until myHost declares game to have ended, keeps guessing and checking
 while myHost.endgame == False:
 	myPlayer.guess(myHost.result)
 	myHost.check(myPlayer.myGuess)
