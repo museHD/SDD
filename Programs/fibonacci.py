155a48aadf9233a1ae85b7b4734307d72f260f30
museHD <57558058+museHD@users.noreply.github.com>
2021-02-13 01:47:37 +1100

    Added sample code for fibonacci

--- Programs/fibonacci.py
+++ Programs/fibonacci.py
@@ -45,5 +45,25 @@ nth num in fib
 fib returns 0+1 = 1
 fib runs
 
+Code found on the internet 
+# Function for nth Fibonacci number
+ 
+def Fibonacci(n):
+    if n<0:
+        print("Incorrect input")
+    # First Fibonacci number is 0
+    elif n==0:
+        return 0
+    # Second Fibonacci number is 1
+    elif n==1:
+        return 1
+    else:
+        return Fibonacci(n-1)+Fibonacci(n-2)
+ 
+# Driver Program
+ 
+print(Fibonacci(9))
+ 
+#This code is contributed by Saket Modi
 
 '''
