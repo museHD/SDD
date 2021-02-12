8dac59bfce3b44331ccc624ed826e67157de2d5a
museHD <57558058+museHD@users.noreply.github.com>
2021-02-13 01:41:32 +1100

    attempting fibonacci recursive

--- Programs/fibonacci.py
+++ Programs/fibonacci.py
@@ -10,14 +10,40 @@ def fibonacci(n):
 		nextnum=final
 		print()
 	print(finalList[n-1])
-fibonacci(4)
+# fibonacci(4)
 
-# initlist = [0,1]
-# tries = 995
-# def fib_rec(n1):
-# 	if n1 == 1:
-# 		return 0
-# 	else:
-# 		return (n1 + fib_rec(n1))
+runs = 0
+n1 = 0
+n2 = 1
 
-# print(fib_rec(2))
+
+def fib_rec(n):
+	global runs, n1, n2
+	runs += 1
+
+	if runs == n+1:
+		return n2
+	elif n == 0:
+		return None
+	elif n == 1:
+		return 0
+
+	temp = n1
+	n1 = n2
+	n2 = temp + n2
+	print(runs,n2)
+	fib_rec(n)
+
+# print(fib_rec(5))
+fib_rec(10)
+
+
+
+
+'''
+nth num in fib
+fib returns 0+1 = 1
+fib runs
+
+
+'''
