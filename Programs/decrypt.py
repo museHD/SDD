dc291a0ce3553b9bc6263dc875f1cff540090ab6
museHD <57558058+museHD@users.noreply.github.com>
2021-02-24 09:47:52 +1100

    Decryption working

--- Programs/decrypt.py
+++ Programs/decrypt.py
@@ -1,4 +1,7 @@
-msg = "Pm ol ohk hufaopun jvumpkluaphs av zhf, ol dyval pa pu jpwoly, aoha pz, if zv johunpun aol vykly vm aol slaalyz vm aol hswohila, aoha uva h dvyk jvbsk il thkl vba."
+# msg = "Pm ol ohk hufaopun jvumpkluaphs av zhf ol dyval pa pu jpwoly aoha pz if zv johunpun aol vykly vm aol slaalyz vm aol hswohila aoha uva h dvyk jvbsk il thkl vba"
+# msg ="Kh jg jcf cpavjkpi eqphkfgpvkcn vq uca jg ytqvg kv kp ekrjgt vjcv ku da uq ejcpikpi vjg qtfgt qh vjg ngvvgtu qh vjg cnrjcdgv vjcv pqv c yqtf eqwnf dg ocfg qwv"
+msg = 'Kxvtja Kjv'
+msg = msg.split(" ")
 letters = [chr(b) for b in range(ord('a'), (ord('z')+1))]
 print(letters)
 greater = []
@@ -8,15 +11,17 @@ moreStr = ''
 # 	print(i)
 # 	moreStr = ''
 # 	lessStr = ''
-for letter in msg:
+# for letter in msg:
+# 	if letter == '\x19':
+# 		print("ok")
 	
-	greater.append((letters[ord(letter)%len(letters)]))
-	smaller.append(str(chr(ord(letter)-7)))
+# 	greater.append((letters[ord(letter)%len(letters)]))
+# 	smaller.append(str(chr(ord(letter)-7)))
+	
 
-
-lessStr = ''.join(smaller)
-moreStr = ''.join(greater)
-lessStr.replace('<0x19>','j')
+# lessStr = ''.join(smaller)
+# moreStr = ''.join(greater)
+# # lessStr.replace('<0x19>','j')
 
 print('HELOOOOO!')
 	# for letter in msg:
@@ -24,5 +29,27 @@ print('HELOOOOO!')
 	# print('HELOOOOO!')
 
 	# print(moreStr)
-print(lessStr)
-print(moreStr)
+# print(smaller)
+# print(greater)
+
+def decrypt(msg,offset):
+	shift = 7-offset
+
+	dec = r""
+	for eachWord in msg:
+		for eachChar in eachWord:
+			if eachChar.isupper():
+				eachChar = eachChar.lower()
+				newL = (letters[(ord(eachChar)+shift)%len(letters)])
+				newL = newL.capitalize()
+				dec += newL
+			else:
+
+				# newL = chr(ord(eachChar)-shift)
+				(ord(eachChar)-shift)%len(letters)
+				newL = (letters[(ord(eachChar)+shift)%len(letters)])
+				dec += newL
+		dec += " "
+	print(dec)
+
+decrypt(msg,9)
