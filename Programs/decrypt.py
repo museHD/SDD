024fbd3795db094731077bfd340d3736061848d3
museHD <57558058+museHD@users.noreply.github.com>
2021-02-25 19:26:42 +1100

    adding comments

--- Programs/decrypt.py
+++ Programs/decrypt.py
@@ -3,25 +3,24 @@ msg = """Fgdzqp uf gb etagxp za hmxxqk oageuz tq. Ebqmwuzs zgyqdage mew pup tadd
 
 Mpyudmfuaz iq egddagzpqp baeeqeeuaz rdqcgqzfxk tq. Dqymdwmnxk pup uzodqmeuzs aoomeuazmx faa ufe purruogxfk rmd qebqoumxxk. Wzaiz fuxqp ngf eaddk vak nmxxe. Nqp egppqz ymzzqd uzpqqp rmf zai rqqnxk. Rmoq pa iuft uz zqqp ar iurq bmup ftmf nq. Za yq mbbxmgpqp ad rmhagdufq pmetiaape ftqdqradq gb puefdgefe qjbxmuzqp. 
 """
+# list of a-z lowercase
 letters = [chr(b) for b in range(ord('a'), (ord('z')+1))]
 
-
-print(letters)
-greater = []
-smaller = []
-moreStr = ''
 import matplotlib.pyplot as plt
 
+# returns decrypted caeser ciphers; anything thats not a letter is ignored
 def decrypt(msg,offset):
 	shift = 7-offset
 	msg = msg.split(" ")
 
 	dec = r""
+	# Goes through each char; handles capitaliisation and ignores char if not part of letters list
 	for eachWord in msg:
 		for eachChar in eachWord:
 			if eachChar.lower() in letters:
 				if eachChar.isupper():
 					eachChar = eachChar.lower()
+					# Modulus used for wraparound on a list
 					newL = (letters[(ord(eachChar)+shift)%len(letters)])
 					newL = newL.capitalize()
 					dec += newL
@@ -33,19 +32,24 @@ def decrypt(msg,offset):
 					dec += newL
 			else:
 				dec += eachChar
+		# space after each word
 		dec += " "
 	return dec
 
+# Decrypts caeser ciphers using simple frequency analysis
 def autodecrypt(msg):
 
 	# /give @p minecraft:diamond_sword{Enchantments:[{id:sharpness,lvl:2000}]}
+
 	freq = []
+	# Go through all letters and store their frequency in the message in a list
 	for eachL in letters:
 		letterCount = msg.count(eachL)
 		freq.append(letterCount)
 
+	# Calculate how much the shift is
 	highestIndex = freq.index(max(freq))
-	shift = highestIndex-4
+	shift = highestIndex-4 # 'e' is the 4th element in letters list
 	print(decrypt(msg,shift))
 	# print()
 	# print(letters[highestIndex])
