6d659bab38120b750832a5df010ced265f870936
museHD <57558058+museHD@users.noreply.github.com>
2021-02-25 14:45:28 +1100

    autodecrypt working

--- Programs/decrypt.py
+++ Programs/decrypt.py
@@ -1,35 +1,16 @@
-# msg = "Pm ol ohk hufaopun jvumpkluaphs av zhf ol dyval pa pu jpwoly aoha pz if zv johunpun aol vykly vm aol slaalyz vm aol hswohila aoha uva h dvyk jvbsk il thkl vba"
-# msg ="Kh jg jcf cpavjkpi eqphkfgpvkcn vq uca jg ytqvg kv kp ekrjgt vjcv ku da uq ejcpikpi vjg qtfgt qh vjg ngvvgtu qh vjg cnrjcdgv vjcv pqv c yqtf eqwnf dg ocfg qwv"
-msg = 'Kxvtja Kjv'
+# msg = 'Vncqxm rw fqrlq njlq unccna rw cqn yujrwcngc rb anyujlnm kh j unccna bxvn orgnm wdvkna xo yxbrcrxwb mxfw cqn juyqjknc. Cqn vncqxm rb wjvnm jocna Sdurdb Ljnbja, fqx dbnm rc rw qrb yarejcn lxaanbyxwmnwln.'
+msg = """Fgdzqp uf gb etagxp za hmxxqk oageuz tq. Ebqmwuzs zgyqdage mew pup taddunxq bmowmsqe eqf. Metmyqp tqdeqxr tme puefmzf omz efgpuqp yde. Xqp ftqdqradq ufe yuppxqfaz bqdbqfgmx rgxruxxqp bdahueuaz rdmzwzqee. Eymxx tq pdmiz mrfqd myazs qhqdk ftdqq za. Mxx tmhuzs ngf kag qpimdp sqzuge ftagst dqymdw azq. 
+
+Mpyudmfuaz iq egddagzpqp baeeqeeuaz rdqcgqzfxk tq. Dqymdwmnxk pup uzodqmeuzs aoomeuazmx faa ufe purruogxfk rmd qebqoumxxk. Wzaiz fuxqp ngf eaddk vak nmxxe. Nqp egppqz ymzzqd uzpqqp rmf zai rqqnxk. Rmoq pa iuft uz zqqp ar iurq bmup ftmf nq. Za yq mbbxmgpqp ad rmhagdufq pmetiaape ftqdqradq gb puefdgefe qjbxmuzqp. 
+"""
 letters = [chr(b) for b in range(ord('a'), (ord('z')+1))]
+
+
 print(letters)
 greater = []
 smaller = []
 moreStr = ''
-# for i in range(10):
-# 	print(i)
-# 	moreStr = ''
-# 	lessStr = ''
-# for letter in msg:
-# 	if letter == '\x19':
-# 		print("ok")
-	
-# 	greater.append((letters[ord(letter)%len(letters)]))
-# 	smaller.append(str(chr(ord(letter)-7)))
-	
-
-# lessStr = ''.join(smaller)
-# moreStr = ''.join(greater)
-# # lessStr.replace('<0x19>','j')
-
-print('HELOOOOO!')
-	# for letter in msg:
-	# 	smaller.append(str(chr(ord(letter)-i)))
-	# print('HELOOOOO!')
-
-	# print(moreStr)
-# print(smaller)
-# print(greater)
+import matplotlib.pyplot as plt
 
 def decrypt(msg,offset):
 	shift = 7-offset
@@ -38,24 +19,39 @@ def decrypt(msg,offset):
 	dec = r""
 	for eachWord in msg:
 		for eachChar in eachWord:
-			if eachChar.isupper():
-				eachChar = eachChar.lower()
-				newL = (letters[(ord(eachChar)+shift)%len(letters)])
-				newL = newL.capitalize()
-				dec += newL
-			else:
+			if eachChar.lower() in letters:
+				if eachChar.isupper():
+					eachChar = eachChar.lower()
+					newL = (letters[(ord(eachChar)+shift)%len(letters)])
+					newL = newL.capitalize()
+					dec += newL
+				else:
 
-				# newL = chr(ord(eachChar)-shift)
-				(ord(eachChar)-shift)%len(letters)
-				newL = (letters[(ord(eachChar)+shift)%len(letters)])
-				dec += newL
+					# newL = chr(ord(eachChar)-shift)
+					(ord(eachChar)-shift)%len(letters)
+					newL = (letters[(ord(eachChar)+shift)%len(letters)])
+					dec += newL
+			else:
+				dec += eachChar
 		dec += " "
-	print(dec)
+	return dec
 
 def autodecrypt(msg):
 
+	# /give @p minecraft:diamond_sword{Enchantments:[{id:sharpness,lvl:2000}]}
+	freq = []
 	for eachL in letters:
 		letterCount = msg.count(eachL)
+		freq.append(letterCount)
+
+	highestIndex = freq.index(max(freq))
+	shift = highestIndex-4
+	print(decrypt(msg,shift))
+	# print()
+	# print(letters[highestIndex])
+	# plt.plot(letters,freq)
+	# plt.show()
 
 
-decrypt(msg,9)
+# decrypt(msg,9)
+autodecrypt(msg)
